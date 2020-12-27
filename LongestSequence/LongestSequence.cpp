adf3798d811288456e259a313fd73e89f52811cc
pinkbluesky <pinkbluesky@users.noreply.github.com>
2020-12-26 17:41:17 -0800

    implement longest seq

--- LongestSequence/LongestSequence.cpp
+++ LongestSequence/LongestSequence.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <ctime> 
+#include <vector>
+#include <algorithm>
+
+/***
+* Given an array of integers, find the length of the longest increasing subsequence, from left to right.
+* Sequence doesn't have to be continuous.
+*/
+
+int const MAXN = 10;
+int const MAXV = 100;
+
+int arr[MAXN];
+int n;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // generate random array and print it out
+    srand((unsigned)std::time(0));
+    n = rand() % MAXN + 1;
+
+    std::cout << "n = " << n << std::endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % MAXV + 1;
+
+        std::cout << arr[i] << ", ";
+    }
+
+    std::cout << std::endl;
+
+    
+    // find the longest increasing sequence
+    std::vector<int> sequences;
+
+    // binary search
+    int cur_seq = 0;
+    for (int i = 0; i < n; i++)
+    {
+        auto it = std::upper_bound(sequences.begin(), sequences.end(), arr[i]);
+
+        // replace this value
+        if (it != sequences.end())
+        {
+            *it = arr[i];
+        }
+        // append to vector
+        else
+        {
+            sequences.push_back(arr[i]);
+        }
+    }
+
+    std::cout << "Length of longest sequence: " << sequences.size() << std::endl;
 }
